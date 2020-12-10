b3a1df8a911f79af365579e3f29c776e5ef061d5
Louis Christner <louis.christner@epitech.eu>
2020-12-10 12:57:13 +0100

    Update Day10 part2 - WIP

--- day10/part2/main.cpp
+++ day10/part2/main.cpp
@@ -1,6 +1,48 @@
+#include <cmath>
 #include <vector>
 #include <iostream>
 #include <algorithm>
+
+size_t factorial(size_t v)
+{
+    return (v == 0 || v == 1) ? 1 : v * factorial(v - 1);
+}
+
+std::vector<size_t> getRemovablesWithoutValues(const std::vector<size_t> &adapters, std::vector<size_t> values = {})
+{
+    std::vector<size_t> removables;
+    std::vector<size_t> cpyAdapters;
+
+    std::copy_if(
+        adapters.begin(),
+        adapters.end(),
+        std::back_inserter(cpyAdapters),
+        [values](const size_t &adapter){
+            return (std::find(values.begin(), values.end(), adapter) == values.end());
+        }
+    );
+
+    for (auto it = cpyAdapters.begin() + 1; it != cpyAdapters.end() - 1; it++) {
+        if ((*(it + 1)) - (*(it - 1)) <= 3) {
+            std::cout << ((removables.size() == 0) ? "" : ", ") << *it;
+            removables.push_back(*it);
+        }
+    }
+
+    std::cout << std::endl;
+    return removables;
+}
+
+void arrangeAdapters(const std::vector<size_t> &adapters)
+{
+    std::vector<size_t> removables =
+        getRemovablesWithoutValues(adapters);
+
+    size_t rSize = removables.size();
+    size_t rPow = std::pow(2, rSize);
+
+    std::cout << rPow << std::endl;
+}
 
 int main(void)
 {
@@ -17,14 +59,7 @@ int main(void)
     highestRatedAdapter = *(adapters.end() - 1);
     adapters.push_back(highestRatedAdapter + 3);
 
-    // size_t differences[3] = {0};
-
-    // for (auto it = adapters.begin() + 1; it != adapters.end(); it++)
-    //     differences[((*it) - (*(it - 1)) - 1)] += 1;
-
-    for (auto it = adapters.begin() + 1; it != adapters.end() - 1; it++)
-        if ((*(it + 1)) - (*(it - 1)) <= 3)
-            std::cout << *it << " removable" << std::endl;
+    arrangeAdapters(adapters);
 
     return 0;
 }
